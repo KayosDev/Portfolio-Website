a0dfae4308544d35c15dcbf34434771f76961e43
KayosDev <devkayos@gmail.com>
2025-04-22 23:26:11 -0400

    Fixed Wepage errors

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
   trailingSlash: true,
   // Serve from GitHub Pages under repo name
   basePath: '/Portfolio-Website',
-  assetPrefix: '/Portfolio-Website/',
+  assetPrefix: '/Portfolio-Website/', // Comment these out for local development
 };
 
 module.exports = nextConfig;
